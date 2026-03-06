02683712a4e29c735eee2ce7350815796e20a238
ntran180 <ntran180@uw.edu>
2026-03-05 17:09:35 -0800

    added storyboard to swift file

---
+++ TaskMomma/Task-StoryBoard/Task-StoryBoardTests/Task_StoryBoardTests.swift
@@ -0,0 +1,17 @@
+//
+//  Task_StoryBoardTests.swift
+//  Task-StoryBoardTests
+//
+//  Created by Nam Tran on 3/5/26.
+//
+
+import Testing
+@testable import Task_StoryBoard
+
+struct Task_StoryBoardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
